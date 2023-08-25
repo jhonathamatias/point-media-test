308e65ea559f9329ebc7e2a58e269590c5d22453
jhonathamatias <jhonathamatias35@gmail.com>
2023-08-25 08:53:11 -0300

    Criado a tela para criar os arquivos

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,151 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useMemo, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+import JSZip from 'jszip';
+import download from 'js-file-download';
+
+import GoogleSheetsApi, { Sheet } from './GoogleSheetsApi';
+
+import { baseStyle, focusedStyle, acceptStyle, rejectStyle } from './styles';
+import './styles.css';
+
+interface HTMLFiles {
+  fileName?: string;
+  content?: string;
+}
+
+const zip = JSZip();
 
 function App() {
+  const [sheet, setSheet] = useState<Sheet[] | null>();
+  const [spreadsheetId, setSpreadsheetId] = useState('');
+  const [htmlFiles, setHtmlFiles] = useState<HTMLFiles[]>();
+
+  const {
+    acceptedFiles,
+    getRootProps,
+    getInputProps,
+    isFocused,
+    isDragAccept,
+    isDragReject
+  } = useDropzone();
+
+  const style = useMemo(() => ({
+    ...baseStyle,
+    ...(isFocused ? focusedStyle : {}),
+    ...(isDragAccept ? acceptStyle : {}),
+    ...(isDragReject ? rejectStyle : {})
+  }), [
+    isFocused,
+    isDragAccept,
+    isDragReject
+  ]);
+
+  useEffect(() => {
+    if (acceptedFiles.length === 0) {
+      return;
+    }
+
+    const reader = new FileReader();
+
+    reader.addEventListener('load', event => {
+      const htmlContent = event.target?.result as string;
+      const files: HTMLFiles[] = [];
+
+      if (!htmlContent) {
+        return;
+      }
+
+      if (!sheet) {
+        return;
+      }
+
+      const textsModelo = sheet[0];
+      const fileNames = sheet[1];
+
+      textsModelo.rows.forEach((row, index) => {
+        files[index] = {
+          content: htmlContent.replace(/<p class="text_modelo">.*<\/p>/, `<p class="text_modelo">${row}</p>`),
+        };
+      });
+
+      fileNames.rows.forEach((row, index) => {
+        files[index] = {
+          ...files[index],
+          fileName: `${row}.html`,
+        };
+      });
+
+      setHtmlFiles(files)
+    });
+
+    reader.readAsText(acceptedFiles[0]);
+  }, [acceptedFiles, sheet]);
+
+  useEffect(() => {
+    if (!spreadsheetId) {
+      return;
+    }
+
+    const googleSheetsApi = new GoogleSheetsApi(spreadsheetId);
+
+    (async () => {
+      const sheet = await googleSheetsApi.getSheetData();
+
+      if (!sheet) {
+        alert('Ocorreu um erro ao buscar a planilha!');
+
+        return;
+      }
+
+      setSheet(sheet);
+      alert('Planilha carregada com sucesso!');
+    })();
+  }, [spreadsheetId]);
+
+  const handleDownloadZip = () => {
+    if (!htmlFiles?.length) {
+      return;
+    }
+
+    htmlFiles.forEach((htmlFile) => {
+      zip.file(htmlFile?.fileName as string, htmlFile.content as string);
+    });
+
+    zip.generateAsync({ type: 'blob' }).then(blobdata => {
+      const zipblob = new Blob([blobdata]);
+
+      download(zipblob, `pointmedia.zip`);
+    });
+  }
+  const files = acceptedFiles.map(file => (
+    <p key={file.name}>
+      {file.name}
+    </p>
+  ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="main">
+        <div className="spreadsheet-search">
+          <input
+            className="spreadsheet-search-input"
+            type="text"
+            placeholder="Cole o spreadsheet id"
+            onChange={e => setSpreadsheetId(e.target.value)}
+          />
+        </div>
+
+        <div {...getRootProps({ style })}>
+          <input {...getInputProps()} />
+          <p>Arraste os arquivos aqui</p>
+        </div>
+        <div className="info-files">
+          <h3>Arquivos:</h3>
+          {files}
+        </div>
+
+        <button className="download" onClick={() => handleDownloadZip()}>Gerar arquivos</button>
+      </div>
     </div>
   );
 }
---
+++ src/styles.ts
@@ -0,0 +1,28 @@
+export const baseStyle = {
+  flex: 1,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: '40px',
+  borderWidth: 2,
+  borderRadius: 2,
+  borderColor: '#eeeeee',
+  borderStyle: 'dashed',
+  backgroundColor: '#453750',
+  color: '#bdbdbd',
+  outline: 'none',
+  transition: 'border .24s ease-in-out',
+  height: '100px',
+};
+
+export const focusedStyle = {
+  borderColor: '#2196f3'
+};
+
+export const acceptStyle = {
+  borderColor: '#00e676'
+};
+
+export const rejectStyle = {
+  borderColor: '#ff1744'
+};
