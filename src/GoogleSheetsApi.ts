66fbcdf4ed2feeee1b44793c2551855c599cd0d4
jhonathamatias <jhonathamatias35@gmail.com>
2023-08-25 12:09:12 -0300

    Removido console.log

--- src/GoogleSheetsApi.ts
+++ src/GoogleSheetsApi.ts
@@ -3,8 +3,6 @@ export interface Sheet {
   rows: string[];
 }
 
-console.log(process.env)
-
 export default class GoogleSheetsApi {
   private static readonly key = process.env.REACT_APP_GOOGLE_SHEETS_API_KEY;
   private static readonly baseUrl = process.env.REACT_APP_GOOGLE_SHEETS_BASE_URL;
